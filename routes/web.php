5335f88eb904dd6924de201bda5558b69eecbe44
Dh3v1n <dhevintandiono123@gmail.com>
2025-10-19 22:57:45 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index'])->name('products');
 
 Route::get('/', function () {
-    return view('index');
+    return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/project', function () {
+    return view('project');
 });
 
-Route::get('/products', function () {
-    return view('products');
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
 });
